4267559a770e98429939e97e881cf6e81746a373
kunjie.zhang <kunjie.zhang@corp.elong.com>
2018-05-30 09:43:53 +0800

    dao层使用mapper注解

--- src/main/java/com/springboot/Server.java
+++ src/main/java/com/springboot/Server.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by kunjie.zhang on 2018/5/26.
  */
 @SpringBootApplication
-@MapperScan("com.springboot.dao")
 public class Server {
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
--- src/main/java/com/springboot/dao/StudentDao.java
+++ src/main/java/com/springboot/dao/StudentDao.java
@@ -2,10 +2,12 @@ package com.springboot.dao;
 
 
 import com.springboot.model.Student;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Created by user on 2018/1/8.
  */
+@Mapper
 public interface StudentDao extends BaseDao<Student, Long> {
 
 }
